366ce03e619ea12e4f7300519649572a6b73d566
Iakov Kosiakov <kalterxad34@gmail.com>
2021-06-20 16:28:15 +0300

    add pipes

--- src/config/discord.config.ts
+++ src/config/discord.config.ts
@@ -1,6 +1,11 @@
-import { DiscordModuleOption } from 'discord-nestjs';
+import {
+  DiscordModuleOption,
+  TransformPipe,
+  ValidationPipe,
+} from 'discord-nestjs';
 
 export const discordConfig: DiscordModuleOption = {
   token: process.env.DISCORD_SECRET,
   commandPrefix: '/',
+  usePipes: [TransformPipe, ValidationPipe],
 };
