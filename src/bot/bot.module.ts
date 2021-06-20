e8d84298ff6cc4cbcbf5d74c2e42618618823af0
Iakov <kalterxad34@gmail.com>
2021-06-20 23:47:25 +0300

    init translate module

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DiscordModule } from 'discord-nestjs';
 
 import { discordConfig } from '../config';
+import { TranslateModule } from '../translate/translate.module';
 import { ChatCommandsController } from './chat-commands.controller';
 
 @Module({
-  imports: [DiscordModule.forRoot(discordConfig)],
+  imports: [DiscordModule.forRoot(discordConfig), TranslateModule],
   controllers: [ChatCommandsController],
 })
 export class BotModule {}
