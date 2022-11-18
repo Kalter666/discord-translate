efae5786ee761ae3c3f2ac4879f5220ba6c0971a
Iakov Kosiakov <kalterxad34@gmail.com>
2022-11-19 01:43:16 +0300

    upd versions

--- src/config/discord.config.ts
+++ src/config/discord.config.ts
@@ -3,11 +3,11 @@ import {
   TransformPipe,
   ValidationPipe,
 } from 'discord-nestjs';
-import { Intents } from 'discord.js';
+import { GatewayIntentBits } from 'discord.js';
 
 export const discordConfig: DiscordModuleOption = {
   token: process.env.DISCORD_SECRET,
   commandPrefix: '/',
   usePipes: [TransformPipe, ValidationPipe],
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 };
